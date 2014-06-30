dea899036752920508406fd69ecf2ca0d505dd34
Rafał Grodziński <rafal@unalignedbyte.com>
2014-06-30 20:36:12 +0100

    Cleaned up the project

---
+++ Super Maze/Source/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Super Maze
+//
+//  Created by Rafał Grodziński on 30.06.2014.
+//  Copyright (c) 2014 UnalignedByte. All rights reserved.
+//
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window!.makeKeyAndVisible()
+        
+        return true
+    }
+    
+}
+
